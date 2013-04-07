7fa2f46a58124bd233675ef0e66f38994cc4fc93
Jason Chionh <jasonchionh@gmail.com>
2013-04-06 20:53:13 -0700

    first project files

---
+++ src/com/example/sqlite_db_words/WordsDBActivity.java
@@ -0,0 +1,15 @@
+package com.example.sqlite_db_words;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordsDBActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
